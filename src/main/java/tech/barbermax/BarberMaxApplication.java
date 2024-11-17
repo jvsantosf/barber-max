f4a721f83efd2788adbf37ccf5ae8e5c2ce75abd
jvsantosf <jv.santospb@gmail.com>
2024-11-16 22:21:19 -0300

    Projeto adicionado ao github

---
+++ src/main/java/tech/barbermax/BarberMaxApplication.java
@@ -0,0 +1,13 @@
+package tech.barbermax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberMaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberMaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/barbermax/BarberMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.barbermax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberMaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
